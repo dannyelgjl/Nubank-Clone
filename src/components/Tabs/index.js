e1bc19e4aa406ab3449bc48928b29d82995cc471
dannyelgjl <dannyelgjl@gmail.com>
2021-03-20 14:32:15 -0300

    feat: ending animation

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -3,24 +3,40 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, TabsContainer, TabItem, TabText } from "./styles";
 
-const Tabs = () => {
+const Tabs = ({ translateY }) => {
   Icon.loadFont();
 
   return (
-    <Container>
+    <Container style={{
+      trasnform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp',
+        })
+      }],
+      opacity: translateY.interpolate({
+        inputRange: [0, 380],
+        outputRange: [1, 0.3],
+        extrapolate: 'clamp',
+      })
+    }} >
       <TabsContainer>
         <TabItem>
           <Icon name="person-add" size={24} color="#fff"/>
           <TabText>Indicar amigos</TabText>
         </TabItem>
+        
         <TabItem>
           <Icon name="chat-bubble-outline" size={24} color="#fff"  />
           <TabText>Cobrar</TabText>
         </TabItem>
+
         <TabItem>
           <Icon  name="arrow-downward" size={24} color="#fff" />
           <TabText>Depositar</TabText>
         </TabItem>
+
         <TabItem>
           <Icon  name="arrow-upward" size={24} color="#fff" />
           <TabText>Indicar amigos</TabText>
--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,38 +1,93 @@
 import React from 'react';
-import { Text, StatusBar } from 'react-native';
+import { Text, StatusBar, Animated } from 'react-native';
+import { PanGestureHandler, State } from "react-native-gesture-handler"
 import Header from "../../components/Header";
 import Tabs from "../../components/Tabs";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Menu from "../../components/Menu";
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
+Icon.loadFont();
+
+
 const Main = () => {
-  Icon.loadFont();
+  let offSet = 0;
+  const translateY = new Animated.Value(0);
+
+  const animetedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  );
+  
+  const onHandlerStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offSet += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offSet);
+        translateY.setOffset(0);
+        offSet = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offSet = opened ? 380 : 0;
+        translateY.setOffset(offSet);
+        translateY.setValue(0);
+      }); 
+    }
+  };
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY}/>
+        <PanGestureHandler
+          onGestureEvent={animetedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{ 
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }]
+           }}>
+            <CardHeader>
+              <Icon  name="attach-money" size={28} color="#666"/>
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 188.888,98</Description>
+              </CardContent>
 
-        <Card>
-          <CardHeader>
-            <Icon  name="attach-money" size={28} color="#666"/>
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-            <CardContent>
-              <Title>Saldo disponível</Title>
-              <Description>R$ 188.888,98</Description>
-            </CardContent>
-
-            <CardFooter>
-              <Annotation>Transferência de R$ 20,00 recebida de Daniel</Annotation>
-            </CardFooter>
-        </Card>
+              <CardFooter>
+                <Annotation>Transferência de R$ 20,00 recebida de Daniel</Annotation>
+              </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs  translateY={translateY} />
       <StatusBar  barStyle="light-content" backgroundColor="#8B10AE"/>
     </Container>
   );
