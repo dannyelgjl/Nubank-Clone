d120f024c9239ea4ed0421d2b00540a767f68548
dannyelgjl <dannyelgjl@gmail.com>
2021-03-20 10:51:18 -0300

    feat: creating styles for the main card

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Text, StatusBar } from 'react-native';
-import { Container } from './styles';
 import Header from "../../components/Header";
 import Tabs from "../../components/Tabs";
 
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
 const Main = () => {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+          </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 188.888,98</Description>
+            </CardContent>
+
+            <CardFooter>
+              <Annotation>Transferência de R$ 20,00 recebida de Daniel</Annotation>
+            </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
       <StatusBar  barStyle="light-content" backgroundColor="#8B10AE"/>
     </Container>
